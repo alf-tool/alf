1827850340e5e193df44804dec08cdb440af8786
Bernard Lambeau <blambeau@gmail.com>
2013-10-15 11:24:05 +0200

    Import optimizer integration tests from alf-test

---
+++ spec/optimizer/test_all.rb
@@ -0,0 +1,38 @@
+require 'rspec'
+require 'alf'
+require 'alf-test'
+
+conn = Alf::Test::Sap.connect(:fake)
+
+Path.dir.glob('*').select{|f| f.directory? }.each do |folder|
+  optimizer = Alf::Optimizer.const_get(folder.basename.to_s.capitalize)
+
+  describe optimizer do
+
+    folder.glob('*.yml').each do |file|
+      context "On #{file.basename}" do
+
+        file.load.each do |query|
+          alf, optimized = query['alf'], query['optimized']
+
+          context "on #{alf}" do
+            let(:expr)    { conn.parse(alf)       }
+            let(:expected){ conn.parse(optimized) }
+
+            subject{ optimizer.new.call(expr) }
+
+            it 'should be optimized as expected' do
+              subject.should eq(expected)
+            end
+
+            it 'should have same heading as the initial expression' do
+              subject.heading.should eq(expr.heading)
+            end
+          end
+        end
+
+      end
+    end
+  end
+
+end
--- tasks/test.rake
+++ tasks/test.rake
@@ -1,6 +1,6 @@
 require "rspec/core/rake_task"
 desc "Run rspec integration tests"
 RSpec::Core::RakeTask.new(:test) do |t|
-  t.pattern = "test/**/test_*.rb"
-  t.rspec_opts = ["--color", "--backtrace", "-Ilib", "-Itest"]
+  t.pattern = "spec/**/test_*.rb"
+  t.rspec_opts = ["--color", "--backtrace", "-Ilib", "-Ispec"]
 end
