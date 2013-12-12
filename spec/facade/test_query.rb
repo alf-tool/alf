83cd23e1eedcb75ac917490d89fad427377cf4c3
Bernard Lambeau <blambeau@gmail.com>
2013-12-12 10:06:09 +0100

    Add integration test about the class observed in relation tuples.

--- spec/facade/test_query.rb
+++ spec/facade/test_query.rb
@@ -9,4 +9,13 @@ describe Alf, "query" do
     subject.heading.should eq(Heading(sid: String, name: String, status: Integer, city: String))
   end
 
+  it 'should have all tuples of exact same class' do
+    clazz = nil
+    subject.each do |tuple|
+      clazz ||= tuple.class
+      tuple.class.object_id.should eq(clazz.object_id)
+    end
+    clazz.should_not be_nil
+  end
+
 end
